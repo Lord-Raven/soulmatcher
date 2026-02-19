b9be40f4931c9728a1019c579b0e7ad579813ed1
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2026-02-19 16:23:44 +0000

    Swapping emotion pack for Cupid.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -157,7 +157,37 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             themeColor: '#FF69B4',
             themeFontFamily: 'Arial, sans-serif',
             voiceId: 'light_male_20s',
-            emotionPack: this.primaryCharacter.partial_extensions?.chub?.expressions?.expressions
+            //emotionPack: this.primaryCharacter.partial_extensions?.chub?.expressions?.expressions
+            emotionPack: {
+                'admiration': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/04b1739a-41fe-4c3e-9d2a-3b958c8779f7/9f96122f-c133-4ea9-aa1f-1d3efecc9c4d.png',
+                'amusement': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/112d1085-6ec0-41cd-a3c4-d52d93d13cc6/a1cc4317-4555-4107-a3f8-4c93a6fa782c.png',
+                'anger': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/583a8240-dcc7-4c51-b5b0-5b82103bd33c/e2d942a5-df98-44dd-97b9-d925a4339562.png',
+                'annoyance': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/e8ed71f6-eee9-407b-9ca6-df4972ab5d55/256be484-751a-4b8d-9f90-da5b4ce291ed.png',
+                'approval': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/bdcd0759-c557-4f9a-af67-998e2a1b1724/47a8f337-f94f-478a-a2c2-1f1bb29d3fc0.png',
+                'caring': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/94cb58a2-fced-482d-8947-c4f1f99336d6/5b543740-1db4-48eb-9ee9-6bdc7ba45bdc.png',
+                'confusion': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/fd40fd26-5429-43b1-a90f-b921f52e9a65/6f3715a1-d143-4e7c-8ddf-c6b01b6596e7.png',
+                'curiosity': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/331556c7-1563-457d-93ae-280fb647a8a9/575032da-7143-4812-bacc-6987f198307d.png',
+                'desire': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/55d663b8-67eb-484d-a878-bfe6d9ad73b5/fd600ec1-c858-4af5-b3d9-f6771fd0fc58.png',
+                'disappointment': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/d7d2da5b-5c35-4f4e-a8f6-8f945e2ef5fd/7132fac5-5a68-4ad1-831c-aaa416db1460.png',
+                'disapproval': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/e06dac38-9eef-47e7-9cb0-dcf4771688a8/ce6b9ac4-666e-45fd-81c3-7813e6aa2727.png',
+                'disgust': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/e8ba75d6-b114-4d11-acbd-f968298b35b2/6bed1e06-2d3b-4357-b3fd-a1b4e6dc611d.png',
+                'embarrassment': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/3b270970-f428-4c7e-b0e5-4fdd5f787205/2029bb29-0c95-437d-b140-9685b7bbfdae.png',
+                'excitement': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/0bd9f1c9-0a53-4912-810d-13c222a93633/b90a3e17-c918-4220-b129-fd211a9b9262.png',
+                'fear': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/74ff72d6-882e-4784-a890-19f59ca034c9/afbb9b32-04e4-4be2-a12c-f4baec356cd8.png',
+                'gratitude': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/97a2eb9f-44cc-4dfb-9fd5-cc5c73d3ca43/6fd0c4b0-42ab-4e5b-85d3-fd444b9ce0af.png',
+                'grief': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/6310cd7d-8453-49d5-9b83-c3b1b97450be/036fdcb4-db7b-4c04-b419-10e5e57653d5.png',
+                'joy': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/c4bd5e05-f02a-45e9-b344-0d92616ba7f0/0bcb022e-49ad-4d38-a755-53ac7a36b364.png',
+                'love': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/9e34db68-9e51-4046-87f6-60f68bb69d8c/8fb1bd2b-daee-4f25-9f99-d7de50913414.png',
+                'nervousness': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/c548994a-30b9-45a4-88da-17830f905f2c/c8435a70-9283-4ce8-85dc-730c70511d39.png',
+                'optimism': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/2af67729-784a-4e15-8d6e-e3df14fab918/04fc7739-dd09-4eca-9bcf-614ae53ef856.png',
+                'pride': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/1ce61830-246c-4453-9f66-9782f88e9f7d/96e8c6ca-f019-402a-945c-968705e4933c.png',
+                'realization': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/f3b26fd7-6f4c-4084-8571-b1697138a765/9d5f7c01-1df2-49fe-85c2-a1faf8b899a5.png',
+                'relief': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/6cc0c3d1-9c38-415e-9d85-2fb95127db09/8248f5f6-c77b-4b45-96a9-cd8a54d5e833.png',
+                'remorse': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/cffb02af-ed95-45dd-9bd8-524100251b45/d6df467a-a055-471b-adab-a70aba3c8129.png',
+                'sadness': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/bae72d23-affe-437b-8d38-d8fcacda9695/80701bab-58d7-4991-9c65-5aafcc88af11.png',
+                'surprise': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/e134e752-95a7-45f2-b534-5fe5bf1315a4/d4693f67-637d-497c-b974-52e9c6a55929.png',
+                'neutral': 'https://avatars.charhub.io/avatars/uploads/images/gallery/file/8aeba0f9-54b7-4970-8ade-04eec70d9adf/d0ffdcaa-d5dc-4925-8cae-87f755c4803e.png',
+            }
         });
 
         console.log('Host actor emotion pack:');
